3c7337ff0e87eae2440b7204a629c5664fef63d4
Matt Glover <matt@duchysoftware.com>
2013-07-23 18:08:23 +0100

    @Added pns.m - Property (nonatomic, strong)

---
+++ pns.m
@@ -0,0 +1,7 @@
+// Property (nonatomic, strong) Classname *propertyName
+// 
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Interface Methods
+
+@property (nonatomic, strong) <#ClassName#> *<#propertyName#>;

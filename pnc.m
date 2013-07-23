79bd3242ad2698b79501aacee2e8095a6e36d218
Matt Glover <matt@duchysoftware.com>
2013-07-23 18:22:50 +0100

    Added pnc.m - Property (nonatomic, copy) NSString

---
+++ pnc.m
@@ -0,0 +1,7 @@
+// Property (nonatomic, copy) NSString
+// 
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Interface Methods
+
+@property (nonatomic, copy) NSString *<#propertyName#>;

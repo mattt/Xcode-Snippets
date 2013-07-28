2426c0b5f4b931ef3517910ed2f3388cd638239c
Zhuo Huang <zfhuang@ucdavis.edu>
2013-07-28 15:46:25 -0700

    snippet for nsenum.

---
+++ nsenum.m
@@ -0,0 +1,11 @@
+// A easy typedef for NS_ENUM 
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Top Level
+
+typedef NS_ENUM(NSInteger, <#TYPE_NAME#>) {
+  <#TYPE0#>,
+  <#TYPE1#>,
+};
+

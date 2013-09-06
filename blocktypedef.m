3f3757bbac448fbd13bbf604f804933900190acc
Terry Lewis <terrylewis@outlook.com>
2013-09-06 12:34:37 -0500

    More block snippets.

---
+++ blocktypedef.m
@@ -0,0 +1,10 @@
+// typedef block declaration.
+// typedef a block.
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Preprocessor Directive
+
+
+
+typedef <#return_type#>(^<#name#>)(<#parameters#>);

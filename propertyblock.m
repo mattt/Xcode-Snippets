f55d0c42ae157dc0b0d161b3f1102aaaa4b8345e
Илья Седов <i.setoff@gmail.com>
2014-12-16 10:29:25 +0700

    method arg block, property block

---
+++ propertyblock.m
@@ -0,0 +1,8 @@
+// Block as class property
+// Define property with block type
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Interface Methods
+
+@property (nonatomic, copy) <#Return Type#> (^<#Property Name#>)(<#Args#>);

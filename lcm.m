3be21eaf128e53ca0968abc53d56d13abf6a4951
cuongtv51 <cuong.tv51@gmail.com>
2015-03-14 04:43:10 +0700

    Add some comment shortcut

---
+++ lcm.m
@@ -0,0 +1,12 @@
+// Long Comment
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Class Interface Methods, Class Implementation
+
+/**
+ *  <#Description#>
+ *  @params <#param#>
+ *  @params <#param#>
+ *  @returns <#result#>
+ */

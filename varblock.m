4452e0a28fd2575014511d731fe46b57e5ec944f
MUISEROUX Fritzgerald <f.muiseroux@gmail.com>
2015-03-16 22:05:16 +0100

    add new snippet
    Simple snippet for block variable declaration

---
+++ varblock.m
@@ -0,0 +1,10 @@
+// Block variable
+// Simple snippet for block variable declaration
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+<#returnType#>(^<#variableName#>)(<#arguments#>) = ^(<#blockaguments#>){
+    <#code#>
+};

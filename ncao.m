ea8c235a25c87e4c9fc42e2d6deb189223621a13
jliao <jliao@linkedin.com>
2015-11-18 20:06:58 +0800

    add NSNotificationCenter snippets

---
+++ ncao.m
@@ -0,0 +1,6 @@
+---
+title: "NSNotificationCenter Add Observer"
+completion-scope: Function or Method
+---
+
+[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(<#selector#>) name:<#name#> object:<#object#>];
